6e3833a36d8ed03fbeaf58e0604e8d810751754c
Adri-Bagas <abagaswitjaksono@gmail.com>
2023-05-09 09:36:10 +0700

    added function to user models

--- lib/models/User.dart
+++ lib/models/User.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
 
 class User{
   late String id;
@@ -7,4 +10,25 @@ class User{
   late String email;
   late String level;
 }
+
+Future<bool> checkAccountOnLogin(String email, String password) async {
+
+  String LoginURL = 'http://localhost:3000/users/login';
+
+  final response = await http.post(
+    Uri.parse(LoginURL),
+    body: {'email': email, 'password': password},
+  );
+  if (response.statusCode == 200) {
+    final token = jsonDecode(response.body)['token'];
+    print(token);
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('jwt', token);
+    return true;
+  }else{
+    return false;
+  }
+}
+
+
 
